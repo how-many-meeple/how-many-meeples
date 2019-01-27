606ac58f21470185e47324dd12700319c8d2778f
Stephen Whalley <stephen.whalley@sky.uk>
2019-01-27 16:03:18 +0000

    Remove unnecessarily url defaults

--- server/config.js
+++ server/config.js
@@ -1,6 +1,4 @@
 const boardGameGeek = new URL('https://gametableproxy.herokuapp.com');
-boardGameGeek.host = 'gametableproxy.herokuapp.com';
-boardGameGeek.protocol = 'https';
 const defaultPath = '/api/v1/';
 
 export {boardGameGeek, defaultPath};
