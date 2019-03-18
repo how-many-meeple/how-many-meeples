c53105d61ca6fdf370a1cf36f3d7e9bc82e610ca
Stephen Whalley <stephen.whalley@sky.uk>
2019-03-18 21:43:11 +0000

    Implement geeklist request

--- server/controllers/getGeekList.js
+++ server/controllers/getGeekList.js
@@ -1,3 +1,14 @@
-export default (req, res) => {
-    res.send('Unimplemented');
-};
+import bggUrl from '../config';
+import makeRequest from '../request';
+
+export default async function createGetGeekListHandler(req, res) {
+    const collectionUrl = bggUrl
+        .segment('geeklist/')
+        .segment(req.params.listId).toString();
+    const requestHeaders = {
+        'BGG_FILTER_PLAYER_COUNT': req.query.numPlayers,
+        'BGG_FILTER_MIN_DURATION': req.query.minDuration,
+        'BGG_FILTER_MAX_DURATION': req.query.maxDuration
+    };
+    res.json(await makeRequest(collectionUrl, requestHeaders));
+}
--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,6 @@
 import createGetCollectionHandler from './controllers/getCollection';
 import express from 'express';
-import getGeekList from './controllers/getGeekList';
+import getGeekListHandler from './controllers/getGeekList';
 import path from 'path';
 
 const DIST_DIR = __dirname;
@@ -8,8 +8,8 @@ const HTML_FILE = path.join(DIST_DIR, 'index.html');
 
 export default (app) => {
 
-    app.get('/bgg/:username/collection', createGetCollectionHandler);
-    app.get('/bgg/:listId/geekList', getGeekList);
+    app.get('/bgg/collection/:username', createGetCollectionHandler);
+    app.get('/bgg/geeklist/:listId', getGeekListHandler);
 
     app.use(express.static(DIST_DIR));
     app.get('*', (req, res) => res.sendFile(HTML_FILE));
