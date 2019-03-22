d900a6dbcdc693ca65fe4eea03a21ecbc6cf65f2
tfo01 <thomas.forryan@sky.uk>
2019-03-22 17:22:39 +0000

    TF - basic random game page

--- client/RandomGame.js
+++ client/RandomGame.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
 
-const RandomGame = () => {
-  return <div>Some random game</div>;
+const RandomGame = ({ match }) => {
+  const { listType, listOption } = match.params;
+
+  console.log({ listType, listOption });
+
+  const [game, setGame] = useState(null);
+
+  useEffect(() => {
+    axios.get(`/bgg/${listType}/${listOption}`)
+      .then(response => setGame(response.data));
+  }, []);
+
+  return game
+    ? <img src={game.image}/>
+    : <div>No game found...</div>;
+};
+
+RandomGame.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      listType: PropTypes.string,
+      listOption: PropTypes.string,
+    }),
+  }),
 };
 
 export default RandomGame;
