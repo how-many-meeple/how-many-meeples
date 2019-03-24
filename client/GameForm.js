8bde9da3f31adfef7129629d03441d0c30c7d641
Stephen Whalley <stephen.whalley@sky.uk>
2019-03-24 14:24:56 +0000

    Add min and max duration

--- client/GameForm.js
+++ client/GameForm.js
@@ -7,32 +7,48 @@ import TextField from '@material-ui/core/es/TextField/TextField';
 
 const GameForm = () => {
     const [listType, setListType] = useState('');
-    const [numPlayers, setNumPlayers] = useState(0);
+    const [numPlayers, setNumPlayers] = useState('');
+    const [minDuration, setMinDuration] = useState('');
+    const [maxDuration, setMaxDuration] = useState('');
 
-    return(
-    <form>
-        <FormControl>
-            <InputLabel htmlFor="listType">Board Game Geek List Type</InputLabel>
-            <Select
-                value={listType}
-                onChange={e => setListType(e.target.value)}
-                inputProps={{
-                    name: 'listType',
-                    id: 'listType'
-                }}
-            >
-                <MenuItem value={'collection'}>Collection</MenuItem>
-                <MenuItem value={'geeklist'}>Geek List</MenuItem>
-            </Select>
-        <TextField
-            id="numberOfPlayers"
-            value={numPlayers}
-            label="Number of Players"
-            onChange={e => setNumPlayers(e.target.value)}
-            type="number"
-            />
-        </FormControl>
-    </form>
+    return (
+        <form>
+            <FormControl>
+                <InputLabel htmlFor="listType">Board Game Geek List Type</InputLabel>
+                <Select
+                    value={listType}
+                    onChange={e => setListType(e.target.value)}
+                    inputProps={{
+                        name: 'listType',
+                        id: 'listType'
+                    }}
+                >
+                    <MenuItem value={'collection'}>Collection</MenuItem>
+                    <MenuItem value={'geeklist'}>Geek List</MenuItem>
+                </Select>
+                <TextField
+                    id="numberOfPlayers"
+                    value={numPlayers}
+                    label="Number of Players"
+                    onChange={e => setNumPlayers(e.target.value)}
+                    type="number"
+                />
+                <TextField
+                    id="minDuration"
+                    value={minDuration}
+                    label="Minimum Duration(minutes)"
+                    onChange={e => setMinDuration(e.target.value)}
+                    type="number"
+                />
+                <TextField
+                    id="maxDuration"
+                    value={maxDuration}
+                    label="Maximum Duration(minutes)"
+                    onChange={e => setMaxDuration(e.target.value)}
+                    type="number"
+                />
+            </FormControl>
+        </form>
     );
 };
 
