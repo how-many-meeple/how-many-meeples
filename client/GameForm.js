4436677840061926b5178cb4d801e384887cabcd
Stephen Whalley <stephen.whalley@sky.uk>
2019-03-24 09:56:04 +0000

    Add list type to game form

--- client/GameForm.js
+++ client/GameForm.js
@@ -1,40 +1,39 @@
 import React, { useState } from 'react';
 import FormControl from '@material-ui/core/FormControl';
-import PropTypes from 'prop-types';
+import InputLabel from '@material-ui/core/es/InputLabel/InputLabel';
+import MenuItem from '@material-ui/core/es/MenuItem/MenuItem';
+import Select from '@material-ui/core/es/Select/Select';
 import TextField from '@material-ui/core/es/TextField/TextField';
-import { withStyles } from '@material-ui/core/styles';
 
-const styles = theme => ({
-    textField: {
-        marginLeft: theme.spacing.unit,
-        marginRight: theme.spacing.unit,
-        width: 200
-    }
-});
-
-const GameForm = ({ classes }) => {
+const GameForm = () => {
+    const [listType, setListType] = useState('');
     const [numPlayers, setNumPlayers] = useState(0);
 
     return(
-    <form className={classes.root}>
-        <FormControl className={classes.formControl}>
-            <TextField
+    <form>
+        <FormControl>
+            <InputLabel htmlFor="listType">Board Game Geek List Type</InputLabel>
+            <Select
+                value={listType}
+                onChange={e => setListType(e.target.value)}
+                inputProps={{
+                    name: 'listType',
+                    id: 'listType'
+                }}
+            >
+                <MenuItem value={'collection'}>Collection</MenuItem>
+                <MenuItem value={'geeklist'}>Geek List</MenuItem>
+            </Select>
+        <TextField
             id="numberOfPlayers"
             value={numPlayers}
             label="Number of Players"
             onChange={e => setNumPlayers(e.target.value)}
             type="number"
-            className={classes.textField}
             />
         </FormControl>
     </form>
     );
 };
 
-GameForm.propTypes = {
-    classes: PropTypes.shape({
-       textField: PropTypes.string
-    })
-};
-
-export default withStyles(styles)(GameForm);
+export default GameForm;
