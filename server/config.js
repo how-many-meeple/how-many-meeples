ed72147fb20f87e2e988225c8fb7ffad9e0c4292
David Long <david.long@racing-leagues.co.uk>
2019-07-10 22:31:27 +0100

    Change service to use port 8080 (#25)

--- server/config.js
+++ server/config.js
@@ -1,3 +1,3 @@
 import UrlAssembler from 'url-assembler';
 
-export default UrlAssembler('http://game-selector.sixfootsoftware.com/');
+export default UrlAssembler('http://game-selector.sixfootsoftware.com:8080/');
