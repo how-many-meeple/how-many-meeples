d6ddfecc79290e88b1f21a6ff2d6f7e627850bc2
Stephen Whalley <41569149+SWH30@users.noreply.github.com>
2019-07-17 11:09:13 +0100

    Prettify form (#29)
    * Centralise the form and increase margins/font size
    
    * Add a header component

--- client/index.js
+++ client/index.js
@@ -1,24 +1,30 @@
 import { BrowserRouter, Route } from 'react-router-dom';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import GameForm from './GameForm';
+import Header from './Header';
 import RandomGameModal from './RandomGameModal';
 import React from 'react';
 import ReactDOM  from 'react-dom';
 
 const theme = createMuiTheme({
     typography: {
-        useNextVariants: true
+        useNextVariants: true,
+        fontSize: 20,
     }
 });
 
 ReactDOM.render(
     <MuiThemeProvider theme={theme}>
         <CssBaseline />
+        <Header/>
             <BrowserRouter>
                 <div>
-                    <Route component={GameForm}/>
-                    <Route path="/random/:listType/:listOption" component={RandomGameModal} />
+                    <Container fixed>
+                        <Route component={GameForm}/>
+                        <Route path="/random/:listType/:listOption" component={RandomGameModal} />
+                    </Container>
                 </div>
             </BrowserRouter>
     </MuiThemeProvider>,
