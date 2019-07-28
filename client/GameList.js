a7e9cd6b274fa47058ab361b2a162636f2146006
Stephen Whalley <stephen.whalley@sky.uk>
2019-07-28 22:27:59 +0100

    Return something if game list is empty

--- client/GameList.js
+++ client/GameList.js
@@ -5,9 +5,19 @@ import List from '@material-ui/core/List';
 import PropTypes from 'prop-types';
 import Typography from '@material-ui/core/Typography';
 import axios from 'axios';
+import {withStyles} from '@material-ui/core';
 
-const GameList = ({ match, location, classes }) => {
-    const { listType, listOption } = match.params;
+const styles = {
+    progress: {
+        marginLeft: 'calc(50% - 100px)',
+    },
+    failed: {
+        textAlign: 'center',
+    },
+};
+
+const GameList = ({match, location, classes}) => {
+    const {listType, listOption} = match.params;
     const queryString = location.search;
     const [gameList, setGameList] = useState([]);
     const [isLoaded, setLoaded] = useState(false);
@@ -16,32 +26,39 @@ const GameList = ({ match, location, classes }) => {
         setGameList([]);
         setLoaded(false);
         axios.get(`/bgg/list/${listType}/${listOption}${queryString}`)
-          .then((response) => {
-            setGameList([...response.data]);
+            .then((response) => {
+                setGameList([...response.data]);
+                setLoaded(true);
+            }).catch(() => {
             setLoaded(true);
-          }).catch(() => {
-            setLoaded(true);
-          });
-      };
+        });
+    };
 
-      useEffect(() => {
+    useEffect(() => {
         getGameList();
-      }, []); // eslint-disable-line react-hooks/exhaustive-deps
+    }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-    return (
-         <div>
-              <h2>List of Compatible Games</h2>
-             <List>
-               {
-               gameList  
-               ? gameList.map((game, index) => <GameListItem key={index} game={game}/> ) 
-               : isLoaded
-                ? <Typography variant="h3" className={classes.failed}>Sorry! Failed to find an appropriate game</Typography>
-                : <CircularProgress className={classes.progress} size={200} />
-               }
-            </List>
-        </div>
-    );
+    if (!gameList.length) {
+        return (<Typography variant="h3" className={classes.failed}>Sorry! Failed to find any appropriate
+            games</Typography>);
+    } else {
+        return (
+            <div>
+                <h2>List of Compatible Games</h2>
+                <List>
+                    {
+                        gameList
+                            ? gameList.map((game, index) => <GameListItem key={index} game={game}/>)
+                            : isLoaded
+                            ? <Typography variant="h3" className={classes.failed}>Sorry! Failed to get list from BGG,
+                                please try again.</Typography>
+                            : <CircularProgress className={classes.progress} size={200}/>
+                    }
+                </List>
+            </div>
+        );
+    }
+
 };
 
 GameList.propTypes = {
@@ -51,14 +68,14 @@ GameList.propTypes = {
     }),
     match: PropTypes.shape({
         params: PropTypes.shape({
-          listType: PropTypes.string,
-          listOption: PropTypes.string,
-          minPlayers: PropTypes.number,
+            listType: PropTypes.string,
+            listOption: PropTypes.string,
+            minPlayers: PropTypes.number,
         }),
-      }),
+    }),
     location: PropTypes.shape({
         search: PropTypes.string,
     }),
 };
 
-export default GameList;
+export default withStyles(styles)(GameList);
